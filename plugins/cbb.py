bbb2237a7ec412af0a619be8ffab2b405b1fd3a2
Rakesh Meena <82395901+rakeshyt@users.noreply.github.com>
2021-10-11 08:23:39 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ 𝐌𝐲 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : <a href='tg://user?id={OWNER_ID}'>🕊️⃝🇮🇳★𝐃𝐞𝐯𝐢𝐋😈𝐇𝐚𝐜𝐤𝐞𝐫★🇮🇳⃝🕊️</a>\n○ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞 : <code>Python3</code>\n○ 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ 𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞 : <a href='https://github.com/rakeshyt/DevilFilesStore'>Click here</a>\n○ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : @JaiHindChatting\n○ 𝐌𝐚𝐢𝐧 𝐆𝐫𝐨𝐮𝐩 : @TheModdingGod</b>",
+            text = f"<b>○ 𝐌𝐲 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 : <a href='tg://user?id={OWNER_ID}'>🕊️⃝🇮🇳★𝐃𝐞𝐯𝐢𝐋😈𝐇𝐚𝐜𝐤𝐞𝐫★🇮🇳⃝🕊️</a>\n○ 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞 : <code>Python3</code>\n○ 𝐋𝐢𝐛𝐫𝐚𝐫𝐲 : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ 𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞 : <a href='https://github.com/rakeshyt/DevilFilesStore'>Click here</a>\n○ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : @JaiHindChatting\n○ 𝐌𝐚𝐢𝐧 𝐆𝐫𝐨𝐮𝐩 : @SonalModdingGod</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
